87cf94b701b7c1794407176d4e9d96af3382976c
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-11 15:08:56 +0200

    pseudocodice

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("fizzbuzz")
+for (i = 0; i < 100; i++)
+
+//stampa i numeri da 1 a 100
+    
+console.log(i+1)
+
+//IF i è un multiplo di 3
+    //stampa Fizz
+
+//ELSE IF i è un multiplo di 5
+    //stampa Buzz
+
+//ELSE IF i è un multiplo sia di 3 che di 5
+    //stampa FizzBuzz
+
+//ELSE se non è nultiplo né di 3 né di 5
+    //stampa i
+
+
+
+
