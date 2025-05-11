503a0682007d85d2877a0dd4dec1cf6441deb2b3
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-11 15:32:12 +0200

    sistemazione

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//creazione ciclo for
+
 for (let i = 0; i < 100; i++){
 
 //stampa i numeri da 1 a 100
@@ -9,21 +11,21 @@ let mlt3 = (i+1)%3
 let mlt5 = (i+1)%5
 
 
-//IF i è un multiplo di 3
-if(mlt3=0)
+//IF i è un multiplo sia di 3 che di 5
+if(mlt3==0 && mlt5==0)
     //stampa Fizz
-console.log("Fizz")
+console.log("FizzBuzz")
 
 //ELSE IF i è un multiplo di 5
 
-else if(mlt=0)
+else if(mlt5==0)
     //stampa Buzz
 console.log("Buzz")
 
-//ELSE IF i è un multiplo sia di 3 che di 5
-else if(mlt3=0 && mlt5=0)
+//ELSE IF i è un multiplo di 3
+else if(mlt3==0)
     //stampa FizzBuzz
-console.log("FizzBuzz")
+console.log("Fizz")
 //ELSE se non è nultiplo né di 3 né di 5
 else
 console.log(i+1)
